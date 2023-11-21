606018f6698c565898a454e7e47a606898d134ba
Robert Nitschke <rdn216@outlook.com>
2023-11-21 17:43:27 +0800

    adding functions to the lists html pages

---
+++ app/models/list.rb
@@ -0,0 +1,9 @@
+class List < ApplicationRecord
+    has_many :bookmarks,  dependent: :destroy
+    has_many :movies, through: :bookmarks
+
+    validates :name, uniqueness: true
+    validates :comment, length: { minimum: 6 }
+
+
+end
